204a45cbf16e500487e62d3c804437f1c876e921
Michael Essam <maichelessam2001@gmail.com>
2025-08-02 21:31:34 +0300

    add files

--- quiz.js
+++ quiz.js
@@ -1,11 +1,8 @@
 function checkAnswer(){
     const correctAnswer = "4";
     const selectedOption = document.querySelector('input[name="quiz"]:checked');
-    if(!selectedOption){
-        document.getElementById("feedback").textContent="Please select an answer."
-    }
     const userAnswer =selectedOption.value;
-    if(userAnswer === correctAnswer){
+    if (userAnswer === correctAnswer){
         document.getElementById("feedback").textContent="Correct! Well done.";
     }else{
         document.getElementById("feedback").textContent="That's incorrect. Try again!";
